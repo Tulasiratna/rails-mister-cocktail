4ba7ff2b56840e5e0447d87347f965497b76ba39
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-09-19 22:23:47 +0200

    rails-mister-cocktail files updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+
+Dose.destroy_all
+Cocktail.destroy_all
+
+# dose_descriptions = ['10cl', '20cl', '30cl']
+
+json = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(json)
+puts ingredients['drinks']
+ingredients['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
+# Dose.create(description: dose_descriptions.random)
+
